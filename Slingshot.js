d1729ec4f05c994aed248914b15f50bf548ea636
pranav008-man <goswamipranav07@gmail.com>
2022-06-16 16:45:53 +0530

    added catapult, lines and base

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        //loading slingshot images
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,11 +20,30 @@ class SlingShot{
     }
 
     display(){
+        //placing the images on the screen
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+
+            //moving the base according to the bird's position
+            if(pointA.x<220){
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            //adding color to the lines
+            stroke(48,22,8);
+            //to draw both lines of the slingshot
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+           image(this.sling3,pointA.x-30,pointA.y-10,15,20);
+            }
+            else{
+            strokeWeight(4);
+            stroke(48,22,8);
+            line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+           image(this.sling3,pointA.x+25,pointA.y-10,15,20);
+            }
         }
     }
     
--- sketch.js
+++ sketch.js
@@ -36,14 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
+    //display the x and y position of the mouse
+    text(mouseX+','+mouseY,mouseX,mouseY);
     Engine.update(engine);
     strokeWeight(4);
     box1.display();
